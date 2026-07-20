5697e013b73068e5a63b79b4f2833e23e29e359a
Soumyajit295 <soumyajit.de20190@gmail.com>
2026-07-20 21:43:29 +0530

    feat: create jwtauth guard and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './modules/auth/jwtAuth.guard';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  setupSwagger(app);
+  const jwtGuard = app.get(JwtAuthGuard);
+  app.useGlobalGuards(jwtGuard);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
